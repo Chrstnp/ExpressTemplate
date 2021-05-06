99a81e8a6b0e5778cda9590519b15966d1f17bb9
Dev <developer@amcordesign.com>
2021-05-06 12:23:18 -0500

    Agregado request con params

--- src/controllers/Orders.controller.ts
+++ src/controllers/Orders.controller.ts
@@ -10,8 +10,14 @@ class OrdersController implements IControllerBase {
   }
 
   public intializeRoutes() {
+    this.router.get(`${this.path}/:id`, this.getPost);
     this.router.get(this.path, this.getAllPosts);
     this.router.post(this.path, this.createAPost);
+  }
+
+  getPost = (request: express.Request, response: express.Response) => {
+    const { id } = request.params;
+    response.send(`here should list order ${id}`);
   }
 
   getAllPosts = (request: express.Request, response: express.Response) => {
